c39b803c097fc0cfe0845cca8095579e1fc58510
Marco Pegoraro <marco.pegoraro@gmail.com>
2016-10-31 17:13:11 +0100

    remote install

---
+++ README.md
@@ -0,0 +1,5 @@
+Install on Mac:
+
+```
+bash <(curl -s https://raw.githubusercontent.com/marcopeg/humble-cli/master/remote-setup.sh)
+```
--- remote-setup.sh
+++ remote-setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-echo "REMOTE SETUP"
+git clone git@github.com:marcopeg/humble-cli.git ~/.humble-cli
+~/.humble-cli/install.sh
