9f1da2e14cda9a2c6953ac1a1273444a36bc9980
Marco Pegoraro <marco.pegoraro@gmail.com>
2016-11-08 15:30:52 +0100

    #20 Move cleanup script -> do cleanup

--- bin/cli-utilities/cleanup.sh
+++ bin/cli-utilities/cleanup.sh
@@ -1,14 +1,14 @@
 
-if [ "$2" == "volumes" ]; then
+if [ "$3" == "volumes" ]; then
     docker volume rm $(docker volume ls -qf dangling=true)
 fi
 
-if [ "$2" == "containers" ]; then
+if [ "$3" == "containers" ]; then
     docker stop $(docker ps -a -q)
     docker rm -f $(docker ps -a -q)
 fi
 
-if [ "$2" == "images" ]; then
+if [ "$3" == "images" ]; then
     docker rmi -f $(docker images -q)
 fi
 
