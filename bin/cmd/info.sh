3e57b27d3d5f28f251e94da5166bf552c636e066
Marco Pegoraro <marco.pegoraro@gmail.com>
2017-07-20 08:31:08 +0200

    Fix `humble info` to show overridden `$HUMBLE_ENV

--- bin/cmd/info.sh
+++ bin/cmd/info.sh
@@ -9,6 +9,15 @@ echo "    -----------------------"
 for VAR in $DECLARED_ENV_VARS; do
     echo "    $VAR"
 done
+
+if [ ! -z "$HUMBLE_FORCED_ENV" ]; then
+    echo ""
+    echo ""
+    echo "    # Forced (via CLI):"
+    echo "    -----------------------"
+    echo "    HUMBLE_ENV=$HUMBLE_FORCED_ENV"
+fi
+
 echo ""
 echo ""
 echo "    # Docker Compose Config"
